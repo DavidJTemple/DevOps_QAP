2eaa577608c8caed499fa283855dd2ab7d759c70
David Temple <96357555+DavidJTemple@users.noreply.github.com>
2022-07-26 15:05:08 -0230

    Add files via upload

---
+++ DevOps_QAP/src/test/java/com/keyin/devops/DevOpsTest.java
@@ -0,0 +1,11 @@
+package com.keyin.devops;
+
+public class DevOpsTest {
+
+    public static void main(String[] args) {
+        DevOps m = new DevOps();
+        System.out.println(m.GetMyDev());
+
+
+    }
+}
